03af9c1431f5c71d526555b11b4cd4d22deea2c2
Ben Brizzi <brizzitime@gmail.com>
2016-09-15 19:48:35 -0400

    added static assets

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var app = express();
 
+app.use("/", express.static("public"));
+
 app.get("/", function(req, res){
-  res.send("__dirname" + "/public/index.html");
+  res.sendFile("__dirname" + "/public/index.html");
 })
 
 app.listen(3001, function(){
