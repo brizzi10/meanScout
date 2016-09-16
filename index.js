523f95b7a37542b793f8b9d2ccfd13e3400a7acc
Ben Brizzi <brizzitime@gmail.com>
2016-09-15 20:37:22 -0400

    set up basic angular router

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var app = express();
 app.use("/", express.static("public"));
 app.use("/", express.static("bower_components"));
 
-app.get("/", function(req, res){
-  res.sendFile("__dirname" + "/public/index.html");
-})
+app.get("/*", function(req, res){
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 app.listen(3001, function(){
   console.log("sup mello mike");
-})
+});
--- public/js/app.js
+++ public/js/app.js
@@ -2,5 +2,23 @@
 
 (function(){
   angular
-  .module("scout",[]);
+  .module("scout",[
+    "ui.router"
+  ])
+  .config(Router);
+
+  Router.$inject = ["$stateProvider", "$locationProvider"];
+  function Router($stateProvider, $locationProvider){
+    $locationProvider.html5Mode(true);
+    $stateProvider
+    .state("main", {
+      url: "/",
+      template: "<h2>This is working</h2>"
+    })
+    .state("test", {
+      url: "/test",
+      template: "<h2>This is also working</h2>"
+    });
+    $urlRouterProvider.otherwise("/");
+  }
 })();
